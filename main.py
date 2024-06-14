55a4fc993fdc6f7107e8c520267d3679d14a8924
lattellie <lattellie@gmail.com>
2024-06-14 14:16:09 +0800

    ete_part1 update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from oemer import ete
-from oemer import eteTemp
-from oemer import eteWithArgs
+from oemer import ete_part1
+from oemer import ete_part2
+import os
 
 # ete.main() 
 eteWithArgs.main('C:/Ellie/ellie2023~2024/iis/oemer/images/newScope0613/1_tch1_doubleSharp.png',dodewarp=False)
-# import os
+
 
 # folder_path = 'images/testing/origin'
 # files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and not f.startswith('1')]
--- oemer/ete_part1.py
+++ oemer/ete_part1.py
@@ -71,45 +71,7 @@ def generate_pred(img_path: str, use_tf: bool = False) -> Tuple[ndarray, ndarray
 
     return staff, symbols, stems_rests, notehead, clefs_keys
 
-
-def polish_symbols(rgb_black_th=300):
-    img = layers.get_layer('original_image')
-    sym_pred = layers.get_layer('symbols_pred')
-
-    img = Image.fromarray(img).resize((sym_pred.shape[1], sym_pred.shape[0]))
-    arr = np.sum(np.array(img), axis=-1)
-    arr = np.where(arr < rgb_black_th, 1, 0)  # Filter background
-    ker = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
-    arr = cv2.dilate(cv2.erode(arr.astype(np.uint8), ker), ker)  # Filter staff lines
-    mix = np.where(sym_pred+arr>1, 1, 0)
-    return mix
-
-
-def register_notehead_bbox(bboxes):
-    symbols = layers.get_layer('symbols_pred')
-    layer = layers.get_layer('bboxes')
-    for (x1, y1, x2, y2) in bboxes:
-        yi, xi = np.where(symbols[y1:y2, x1:x2]>0)
-        yi += y1
-        xi += x1
-        layer[yi, xi] = np.array([x1, y1, x2, y2])
-    return layer
-
-
-def register_note_id() -> None:
-    symbols = layers.get_layer('symbols_pred')
-    layer = layers.get_layer('note_id')
-    notes = layers.get_layer('notes')
-    for idx, note in enumerate(notes):
-        x1, y1, x2, y2 = note.bbox
-        yi, xi = np.where(symbols[y1:y2, x1:x2]>0)
-        yi += y1
-        xi += x1
-        layer[yi, xi] = idx
-        notes[idx].id = idx
-
-
-def extract(img_path: str, output_path: str, dodewarp: bool) -> str:
+def extract(img_path: str, output_path: str, dodewarp: bool,save_npy: bool) -> str:
     img_path = Path(img_path)
     f_name = os.path.splitext(img_path.name)[0]
     pkl_path = img_path.parent / f"{f_name}.pkl"
@@ -149,86 +111,15 @@ def extract(img_path: str, output_path: str, dodewarp: bool) -> str:
     # Register predictions
     symbols = symbols + clefs_keys + stems_rests
     symbols[symbols>1] = 1
-    layers.register_layer("stems_rests_pred", stems_rests)
-    layers.register_layer("clefs_keys_pred", clefs_keys)
-    layers.register_layer("notehead_pred", notehead)
-    layers.register_layer("symbols_pred", symbols)
-    layers.register_layer("staff_pred", staff)
-    layers.register_layer("original_image", image)
+    dataDict = dict()
+    dataDict['stem_rest'] = stems_rests
+    dataDict['clefs_keys'] = clefs_keys
+    dataDict['staff'] = staff
+    dataDict['notehead'] = notehead
+    dataDict['symbols'] = symbols
+    dataDict['image'] = image
 
-    # ---- Extract staff lines and group informations ---- #
-    logger.info("Extracting stafflines")
-    staffs, zones = staff_extract()
-    layers.register_layer("staffs", staffs)  # Array of 'Staff' instances
-    layers.register_layer("zones", zones)  # Range of each zones, array of 'range' object.
-
-    # ---- Extract noteheads ---- #
-    logger.info("Extracting noteheads")
-    notes = note_extract()
-    notes2 = note_extract()
-
-    # Array of 'NoteHead' instances.
-    layers.register_layer('notes', np.array(notes))
-
-    # Add a new layer (w * h), indicating note id of each pixel.
-    layers.register_layer('note_id', np.zeros(symbols.shape, dtype=np.int64)-1)
-    register_note_id()
-
-    # ---- Extract groups of note ---- #
-    logger.info("Grouping noteheads")
-    groups, group_map = group_extract()
-    layers.register_layer('note_groups', np.array(groups))
-    layers.register_layer('group_map', group_map)
-
-    # ---- Extract symbols ---- #
-    logger.info("Extracting symbols")
-    barlines, clefs, sfns, rests = symbol_extract()
-    layers.register_layer('barlines', np.array(barlines))
-    layers.register_layer('clefs', np.array(clefs))
-    layers.register_layer('sfns', np.array(sfns))
-    layers.register_layer('rests', np.array(rests))
-
-    # ---- Parse rhythm ---- #
-    logger.info("Extracting rhythm types")
-    rhythm_extract()
-
-    sectionDir = 'images/testing/'+f_name+'/'
-    if not os.path.isdir(sectionDir):
-        os.makedirs(sectionDir)
-        print(f"folder {sectionDir} created")
-    if not os.path.isdir('images/testing/mergeImg/'):
-        os.makedirs('images/testing/mergeImg/')
-    mainDir = 'images/testing/mergeImg/'+f_name
-    clefsfnrestImg = createImg('clefsfnrest',image,clefs,sfns,rests,sectionDir)
-    notesfnImg = createImg('notesfn',image,notes,'','',sectionDir)
-    notesImg = createImg('notes',image,notes,'','',sectionDir)
-    staffbarlineImg = createImg('staffbarline',image,staffs,barlines,'',sectionDir)
-    
-    if clefsfnrestImg.shape == notesfnImg.shape == notesImg.shape == staffbarlineImg.shape:
-        x = np.hstack((clefsfnrestImg,notesfnImg,notesImg,staffbarlineImg))
-        cv2.imwrite(mainDir+'.jpg',x)
-        print(f'image {mainDir}.jpg created')
-    else:
-        print('4 images have different size, cannot merge')
-
-    # ---- Build MusicXML ---- #
-    logger.info("Building MusicXML document")
-    basename = os.path.basename(img_path).replace(".jpg", "").replace(".png", "")
-    builder = MusicXMLBuilder(title=basename.capitalize())
-    builder.build()
-    xml = builder.to_musicxml()
-
-    # ---- Write out the MusicXML ---- #
-    out_path = output_path
-    if not out_path.endswith(".musicxml"):
-        # Take the output path as the folder.
-        out_path = os.path.join(out_path, basename+".musicxml")
-
-    with open(out_path, "wb") as ff:
-        ff.write(xml)
-
-    return out_path
-
+    return dataDict
 
 def download_file(title: str, url: str, save_path: str) -> None:
     resp = urllib.request.urlopen(url)
@@ -246,8 +137,7 @@ def download_file(title: str, url: str, save_path: str) -> None:
         print(f"{title}: 100% {length}/{length}"+" "*20)
 
 
-def main(img_path,outputPath = "./images", dodewarp=False) -> None:
-
+def runModel1(img_path,outputPath = "./images", dodewarp=False, save_npy=True) -> dict:
     if not os.path.exists(img_path):
         raise FileNotFoundError(f"The given image path doesn't exists: {img_path}")
 
@@ -263,10 +153,6 @@ def main(img_path,outputPath = "./images", dodewarp=False) -> None:
             download_file(title, url, save_path)
 
     clear_data()
-    mxl_path = extract(img_path,outputPath, dodewarp)
-    img = teaser()
-    img.save(mxl_path.replace(".musicxml", "_teaser.png"))
+    dataDict = extract(img_path,outputPath, dodewarp,save_npy)
+    return dataDict
 
-
-if __name__ == "__main__":
-    main()
